ebd8dad03dcf23c74407e91a816d73aea8a019b0
Jarosław Rauza <jarek@evilus.sytes.net>
2017-10-01 18:30:06 +0200

    Fixed one bug in blog.inc.php that forced php to stop script

--- blog.inc.php
+++ blog.inc.php
@@ -6,7 +6,7 @@ class Blog
     public $name = array();
     public $author = array();
     public $date = array();
-    public disqus_domain_name = "mydisqus";
+    public $disqus_domain_name = "mydisqus";
     public $blog_entries = 1;
     public $blog_entries_per_page = 5;
 }
